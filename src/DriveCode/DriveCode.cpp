4215bf1e712a01cc8536677b345dbf46183d52f3
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2021-03-04 23:55:29 -0500

    Created DriveCode
    Created chassis class for X-drive and regular drive

---
+++ src/DriveCode/DriveCode.cpp
@@ -0,0 +1,20 @@
+#include "vex.h"
+
+
+/*
+
+ Create a chassis class that: 
+ Takes 4 motor objects in its constructor
+ Configure them for X-Drive/Regular drive as specified (so we can call chassis.xDrivePower(), chassis.regDrivePower())
+  *no need for loops here 
+ Provide basic auton functions that move to a given X, Y (for X-drive) and move a certain distance forward(for reg drive). Make a wheel constant for measuring  distance
+
+ *this'll be used as a base for all classes involving the chassis
+ *document so we can understand your thinking and give feedback
+
+*/
+
+class chassis(){
+
+
+}
