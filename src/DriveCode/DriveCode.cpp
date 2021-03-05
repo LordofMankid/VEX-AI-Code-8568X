a8b0a492ddb792af3cf63dbc7e8f7650d069558d
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2021-03-05 00:06:35 -0500

    Update DriveCode.cpp

--- src/DriveCode/DriveCode.cpp
+++ src/DriveCode/DriveCode.cpp
@@ -5,16 +5,23 @@
 
  Create a chassis class that: 
  Takes 4 motor objects in its constructor
- Configure them for X-Drive/Regular drive as specified (so we can call chassis.xDrivePower(), chassis.regDrivePower())
-  *no need for loops here 
+ Configure them for X-Drive/Regular drive with a given input xPower(for xdrive), yPower, turnPower 
+  *(something that looks like function chassis.setXdrivePower(input), chassis.setRegDrivePower(input))
+  *a lot of functions will use this class to set the speed of the drive train so this is important
+ Can set brake modes for the motors
  Provide basic auton functions that move to a given X, Y (for X-drive) and move a certain distance forward(for reg drive). Make a wheel constant for measuring  distance
+  *more for practice so no stress here
 
- *this'll be used as a base for all classes involving the chassis
  *document so we can understand your thinking and give feedback
 
 */
 
 class chassis(){
 
+
+}
+
+
+class intake(){
 
 }
