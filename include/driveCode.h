1e1956d883f835362983ef1b74f9c38bdc611412
kohlkoenig <58374778+LordofMankid@users.noreply.github.com>
2021-03-12 09:44:59 -0500

    Update driveCode.h

--- include/driveCode.h
+++ include/driveCode.h
@@ -1,17 +1,17 @@
 #pragma once
 
 using namespace vex;
-
+/*
 class chassis
 {
   private:
   static motor FrontLeft, FrontRight, BackLeft, BackRight;
   public:
   chassis(motor FrontLeftMotor, motor FrontRightMotor, motor BackLeftMotor, motor BackRightMotor){    };
-
+  void set_xDrive(int yPower, int xPower, int rPower) { };
 
 };
-
+*/
 
 
 
